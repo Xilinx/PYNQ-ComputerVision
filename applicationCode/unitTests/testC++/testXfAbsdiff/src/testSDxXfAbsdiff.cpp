f80811528aa25df4da246437a30d420ef49b852e
Kristof Denolf <kristof@xilinx.com>
2019-08-05 15:38:31 -0700

    fix includes

--- applicationCode/unitTests/testC++/testXfAbsdiff/src/testSDxXfAbsdiff.cpp
+++ applicationCode/unitTests/testC++/testXfAbsdiff/src/testSDxXfAbsdiff.cpp
@@ -33,88 +33,143 @@
 /*****************************************************************************
 *
 *     Author: Kristof Denolf <kristof@xilinx.com>
-*     Date:   2018/05/14
+*     Date:   2018/03/13
 *
 *****************************************************************************/
-
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <opencv2/core/core.hpp>
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
 #include <stdexcept>
 
-#include "OpenCVUtils.h"
-#include "HRTimer.h"
+#include <OpenCVUtils.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "xfSDxAbsdiff.h"
+
+//include xF::mat prototype
+#include <Mat/inc/mat.hpp>
+#include <HRTimer.h>
 
 using namespace cv;
+
+//PL instantiation parameters
+#include "PLInstantiationParameters.h"
 
 /** @function main */
 int main ( int argc, char** argv )
 {
-  //command line parsing
-  const String keys =
-  "{help h usage ? |                 | print this message                   }"
-  "{@image	       |                 | in image 	                        }"  
-  "{@image	       |                 | in image 	                        }"
-  "{display d      |                 | diplay result with imshow            }"
-  "{interations n  | 1               | number of iterations to measure time }"
-  ;
+	HRTimer timer;
 
-  CommandLineParser parser(argc, argv, keys);
-  if (parser.has("help") || argc < 2)
-  {
-    parser.printMessage();
-    std::cout << "\nhit enter to quit...";
-    std::cin.get();
-    return 0;
-  }
+  	// Command line parsing
+	const String keys =
+		"{help h usage ? |                 | print this message                   }"
+		"{@image         |                 | input image #1	                      }"
+		"{@image	     |                 | input image #2                       }"
+		"{goldenFile gf  |                 | golden output image (from SW)        }"
+		"{outFile of     |                 | output image (from PL)               }"
+		"{display d      |                 | diplay result with imshow            }"
+		"{iterations n   | 1               | number of iterations to measure time }"
+		;
 
-  String fileName1 = parser.get<String>(0);   
-  String fileName2 = parser.get<String>(1); 
-  
-  unsigned int numberOfIterations = 0;
-  if (parser.has("interations"))
-  numberOfIterations = parser.get<unsigned int>("interations");
-
-  bool imShowOn = parser.has("display");
-
-  /// Declare variables
-  Mat src1, src2;
-  Mat gray1, gray2;   
-  Mat dstSW;
-
-  // Initialize
-  initializeSingleImageTest(fileName1, src1);
-  initializeSingleImageTest(fileName2, src2);
-  
-  cvtColor(src1, gray1, COLOR_BGR2GRAY, 1);
-  cvtColor(src2, gray2, COLOR_BGR2GRAY, 1);
-  
-  if (numberOfIterations > 0) {
-    HRTimer timer;
-
-	std::cout << "starting time measurement: " << numberOfIterations << std::endl;
-    timer.StartTimer();
+	CommandLineParser parser(argc, argv, keys.c_str());
+	if (parser.has("help") || argc < 2)
+	{
+		parser.printMessage();
+		std::cout << "\nhit enter to quit...";
+		std::cin.get();
+		return 0;
+	}
 	
-    for (unsigned int i = 0; i < numberOfIterations; i++) {
-		absdiff(src1, src2, dstSW);
+	String fileName1 = parser.get<String>(0);  
+	String fileName2 = parser.get<String>(1); 
+
+	String filenameSW; bool writeSWResult = false;
+	if(parser.has("goldenFile")) {
+		filenameSW = parser.get<String>("goldenFile");
+    	writeSWResult = true;
 	}
 
-    timer.StopTimer();
+	String filenamePL; bool writePLResult = false;
+	if(parser.has("outFile")) {
+		filenamePL = parser.get<String>("outFile");
+    	writePLResult = true;
+	}
 
-    std::cout << "Elapsed time over " << numberOfIterations << " calls: " << timer.GetElapsedUs() << " us or " << (float)timer.GetElapsedUs() / (float)numberOfIterations << "us per frame" << std::endl;
-    std::cout << "Frame rate: " << (float)numberOfIterations / ((float)timer.GetElapsedUs() / 1e6) << std::endl;
-  }
+	bool imShowOn = parser.has("display");
+	unsigned int numberOfIterations;
+	if (parser.has("iterations"))
+		numberOfIterations = parser.get<unsigned int>("iterations");
 
-  if (imShowOn) { 
-	imshow("input src1",src1);	
-	imshow("input src2",src2);
-	imshow("absdiff Results",dstSW);
-	waitKey(0);
-  }
+	if(!parser.check())
+	{
+		parser.printErrors();
+		return(-1);
+	}
+	// Declare variables
+	Mat src1, src2, src1InY, src2InY; 
+	Mat dstSW;
+	
+	// Initialize
+	initializeSingleImageTest(fileName1, src1);
+	initializeSingleImageTest(fileName2, src2);
 
-  return 0;
+	int width = src1.size().width;
+	int height = src1.size().height;
+	  
+	// Declare variables used for HW-SW interface to achieve good performance
+	xF::Mat src1HLS(height, width, CV_8UC1);
+	xF::Mat src2HLS(height, width, CV_8UC1);
+	xF::Mat dstHLS(height, width, CV_8UC1);
+	
+	//convert 3-channel image into 1-channel image
+	cvtColor(src1, src1InY, COLOR_BGR2GRAY, 1);	
+	cvtColor(src2, src2InY, COLOR_BGR2GRAY, 1);
+	//cvtColor(src1, src1HLS, COLOR_BGR2GRAY, 1);	
+	//cvtColor(src2, src2HLS, COLOR_BGR2GRAY, 1);
+	src1HLS.copyFromCvMat(src1InY);
+	src2HLS.copyFromCvMat(src2InY);
+
+	// Apply OpenCV reference Absdiff
+	std::cout << "running golden model" << std::endl;
+	timer.StartTimer();
+	for (int i = 0; i < numberOfIterations; i++){
+		cv::absdiff(src1InY, src2InY, dstSW);
+	}
+	timer.StopTimer();
+	std::cout << "Elapsed time over " << numberOfIterations << "SW call(s): " << timer.GetElapsedUs() << " us or " << (float)timer.GetElapsedUs() / (float)numberOfIterations << "us per frame" << std::endl;
+ 
+	// Call wrapper for xf::Absdiff
+	std::cout << "running hardware Absdiff" << std::endl;
+	timer.StartTimer();
+	for (int i = 0; i < numberOfIterations; i++){  
+		xF::absdiff(src1HLS, src2HLS, dstHLS);
+	}
+	timer.StopTimer();	
+	std::cout << "Elapsed time over " << numberOfIterations << "PL call(s): " << timer.GetElapsedUs() << " us or " << (float)timer.GetElapsedUs() / (float)numberOfIterations << "us per frame" << std::endl;
+
+	// compare results
+	std::cout << "comparing HLS versus golden" << std::endl;
+	int numberOfDifferences = 0;
+	double errorPerPixel = 0;
+	imageCompare(dstHLS, dstSW, numberOfDifferences, errorPerPixel, true, false);
+	std::cout << "number of differences: " << numberOfDifferences << " average L2 error: " << errorPerPixel << std::endl;
+
+	//write back images in files
+	if (writeSWResult)
+		writeImage(filenameSW, dstSW);
+	if (writePLResult)
+		writeImage(filenamePL, dstHLS);
+
+	// Output input and filter output
+	if (imShowOn) {
+		imshow("Input image #1", src1HLS);		
+		imshow("Input image #2", src2HLS);
+		imshow("Processed (SW)", dstSW);
+		imshow("Processed (PL)", dstHLS);
+		waitKey(0);
+	}
+
+	return 0;
 }
